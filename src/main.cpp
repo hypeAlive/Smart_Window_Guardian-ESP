e11e8d876e6338679292b1436366920af75d7e02
Alive <nicolasfr002@gmail.com>
2025-01-22 03:08:18 +0100

    Add SPIFFS support and read a file from the storage partition
    This commit introduces the SPIFFS filesystem, enabling file storage and retrieval. A new `partitions.csv` file defines the storage partition layout. The code is updated to initialize SPIFFS, read the `hello.txt` file from the SPIFFS partition, and log its contents.

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,72 @@
 #include <sys/types.h>
 #include "Config.h"
 #include "HttpServer.h"
+#include "esp_spiffs.h"
+
+static const char *TAG = "example";
+
+static void read_hello_txt(void)
+{
+
+    ESP_LOGI(TAG, "Reading hello.txt");
+
+    // Open for reading hello.txt
+    FILE* f = fopen("/spiffs/hello.txt", "r");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "Failed to open hello.txt");
+        return;
+    }
+
+    char buf[64];
+    memset(buf, 0, sizeof(buf));
+    fread(buf, 1, sizeof(buf), f);
+    fclose(f);
+
+    // Display the read contents from the file
+    ESP_LOGI(TAG, "Read from hello.txt: %s", buf);
+}
 
 extern "C" {
 void app_main() {
+    ESP_LOGI(TAG, "Initializing SPIFFS");
+
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = "/spiffs",
+        .partition_label = NULL,
+        .max_files = 5,
+        .format_if_mount_failed = false
+      };
+
+    // Use settings defined above to initialize and mount SPIFFS filesystem.
+    // Note: esp_vfs_spiffs_register is an all-in-one convenience function.
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        } else if (ret == ESP_ERR_NOT_FOUND) {
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(NULL, &total, &used);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+    } else {
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    }
+
+    /* The following calls demonstrate reading files from the generated SPIFFS
+     * image. The images should contain the same files and contents as the data directory.
+     */
+
+    // Read and display the contents of a small text file (hello.txt)
+    read_hello_txt();
+
     Config& config = Config::getInstance();
 
     WiFiManager wifiManager;
